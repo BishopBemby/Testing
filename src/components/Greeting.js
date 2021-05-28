0d2b295a2a831cac31589964069f1fb7c0db8d59
Bishop Bemby <bishopbemby1995@gmail.com>
2021-05-29 01:57:15 +0530

    testing with states

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Greeting = () => {
+
+    const [textChange, setTextChange] = useState();
+
+    const changeText = (event)=>{
+        setTextChange(true);
+    }
+
     return (
         <div>
            <h2>Hello World</h2> 
-           <p>How are you?</p>
+           {!textChange && <p>How are you?</p>}
+           {textChange && <p>Is your family ok?</p>}
+           <button onClick={changeText}>Change Text</button>
         </div>
     )
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,12 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
 
 
 describe('Greeting text', ()=>{
-    test('show hello word text', ()=>{
-        //Arrange
+
+    beforeEach(()=>{
+        //Arrange before running each test
         render(<Greeting/>);
-    
+    })
+    test('show "hello word" text', ()=>{        
         //Act
         //nothing
     
@@ -15,5 +18,34 @@ describe('Greeting text', ()=>{
         const hello = screen.getByText("Hello World", {exact: false});
     
         expect(hello).toBeInTheDocument();
+    })
+
+    test('renders "How are you?" text', ()=>{
+        const hru = screen.getByText("How are you?", {exact: false});
+        expect(hru).toBeInTheDocument();
+    })
+
+    test('renders "Is your family ok?" text', ()=>{
+        //can be text too
+        const buttonElement = screen.getByRole('button');
+
+        //act
+        userEvent.click(buttonElement);
+
+        //assert
+        const outputElement =  screen.getByText("Is your family ok?", {exact: true});
+        expect(outputElement).toBeInTheDocument();
+    })
+
+    test('not visible "How are you?" text after button click', ()=>{
+        //can be text too
+        const buttonElement = screen.getByRole('button');
+
+        //act
+        userEvent.click(buttonElement);
+
+        //assert
+        const outputElement =  screen.queryByText("How are you?", {exact: true});
+        expect(outputElement).toBeNull();
     })
 })
